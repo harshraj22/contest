9bec4cd595127f70b2be0e5375c0e102335e5e0b
harshraj22 <harshraj22aug@gmail.com>
2019-10-11 00:04:52 +0530

    fixed typo in dashboard.php

--- CSE-Hub/user/dashboard.php
+++ CSE-Hub/user/dashboard.php
@@ -6,7 +6,7 @@
     if(!isset($_GET['user'])){
         echo <<< _END
             <div>
-                <h1>Error 4040 <br> <h3>The page you requested doesn't exists.</h3></h1>
+                <h1>Error 404 <br> <h3>The page you requested doesn't exists.</h3></h1>
             </div>
         _END;
         exit;
