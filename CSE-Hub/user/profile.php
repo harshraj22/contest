d280fa4b1990c00fe1f86d538485490f4b0db12d
janhaviDadhania <janhavivdadhania@gmail.com>
2019-10-24 22:24:36 +0530

    user stats

--- CSE-Hub/user/profile.php
+++ CSE-Hub/user/profile.php
@@ -20,7 +20,17 @@ if(!$conn)
     die("Error while fetching data. Please try after sometimes. <br>".mysqli_connect_error());
 
 $query = "SELECT * FROM user_info WHERE username='{$currentUserId}'";
+$query_wa = "SELECT COUNT(result) FROM user_submissions WHERE result LIKE 'WA'";
+$query_ac = "SELECT COUNT(result) FROM user_submissions WHERE result LIKE 'AC'";
+$query_tle = "SELECT COUNT(result) FROM user_submissions WHERE result LIKE 'TLE'";
+$query_pa = "SELECT COUNT(result) FROM user_submissions WHERE result LIKE 'PA'";
+$query_re = "SELECT COUNT(result) FROM user_submissions WHERE result LIKE 'RE'";
 $result = mysqli_query($conn,$query);
+$result_wa = mysqli_query($conn,$query_wa);
+$result_ac = mysqli_query($conn,$query_ac);
+$result_tle = mysqli_query($conn,$query_tle);
+$result_pa = mysqli_query($conn,$query_pa);
+$result_re = mysqli_query($conn,$query_re);
 
 if(!$result)
     die("Error fetching details of {$currentUserId}<br>".mysqli_error($conn));
@@ -29,9 +39,41 @@ else if(mysqli_num_rows($result) == 0){
     die($noUserMessage);
 }
 
+if(!$result_wa || !$result_ac || !$result_tle || !$result_pa || !$result_re)
+    die("Error <br>".mysqli_error($conn));
+else if(mysqli_num_rows($result) == 0){
+    $noUserMessage = "<div> error<br/> </div>";
+    die($noUserMessage);
+}
+
 $row = mysqli_fetch_row($result);
+$wa = mysqli_fetch_row($result_wa);
+$ac = mysqli_fetch_row($result_ac);
+$tle = mysqli_fetch_row($result_tle);
+$pa = mysqli_fetch_row($result_pa);
+$re = mysqli_fetch_row($result_re);
 
+//echo $wa[0];
+//echo $ac[0];
+//echo $tle[0];
+//echo $pa[0];
+//echo $re[0];
 
+$php_data_array = Array();
+$php_data_array[0][0]="wa";
+$php_data_array[0][1]=$wa[0];
+$php_data_array[1][0]="ac";
+$php_data_array[1][1]=$ac[0];
+$php_data_array[2][0]="tla";
+$php_data_array[2][1]=$tle[0];
+$php_data_array[3][0]="pa";
+$php_data_array[3][1]=$pa[0];
+$php_data_array[3][0]="re";
+$php_data_array[3][1]=$re[0];
+
+echo "<script>
+        var my_2d = ".json_encode($php_data_array)."
+</script>";
 ?>
 
 <!DOCTYPE html>
@@ -77,8 +119,8 @@ $row = mysqli_fetch_row($result);
             <br/>
             submissions
             <br/>
-            pi chart of Submissions
-            and... space to represent other user stats
+            <div id="chart_div"></div>
+            <br><br>
         </section>
     </section>
 
@@ -86,4 +128,22 @@ $row = mysqli_fetch_row($result);
         <p>This is the footer element.</p>
     </footer>
 </body>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="https://cdn.anychart.com/js/8.0.1/anychart-core.min.js"></script>
+<script>
+google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(draw_my_chart);
+    function draw_my_chart() {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'language');
+        data.addColumn('number', 'Nos');
+		for(i = 0; i < my_2d.length; i++)
+    data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
+    var options = {title:'stats',
+                       width:600,
+                       height:500};
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+    }
+</script>
 </html>
